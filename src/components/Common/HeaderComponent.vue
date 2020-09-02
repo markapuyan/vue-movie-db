e11dbdc7486cbcabbce0954219616b324d1fb611
Mark <markapuyanph@gmail.com>
2020-09-02 10:51:17 +0800

    [ENHANCEMENT] Modify Header Component

--- src/components/Common/HeaderComponent.vue
+++ src/components/Common/HeaderComponent.vue
@@ -2,6 +2,7 @@
     <b-navbar>
         <template slot="brand">
             <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <b class="movie-brand-tag">MovieDb</b>
             </b-navbar-item>
         </template>
     </b-navbar>
@@ -12,3 +13,14 @@ export default {
     
 }
 </script>
+
+<style scoped>
+.navbar {
+    background: rgb(128,202,50);
+}
+.movie-brand-tag {
+    text-decoration: none;
+    color: #FFF;
+    text-transform: uppercase;
+}
+</style>
